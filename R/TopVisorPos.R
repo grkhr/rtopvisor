5ad0e9dea50bf20d8d757e9d18e28fbf111bd87b
grkhr <eduardgorkh@gmail.com>
2019-04-27 00:09:20 +0300

    upd

--- R/TopVisorPos.R
+++ R/TopVisorPos.R
@@ -19,19 +19,28 @@
 #' @examples
 #' TopVisorPos()
 
+
+# "2018-01-01"
+# date1 = "2018-01-01"
+date1 = Sys.Date() - 30
+date2 = Sys.Date() - 3
+user_id = "29761"
+token = "f04d5d05699088103a03"
+project_id = "2402037"
+
 TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 = NULL, date2 = NULL)
 {
   library(data.table)
   token <- paste0("bearer ", token)
   date1 = as.character(date1)
   date2 = as.character(date2)
-  
+
   body = toJSON(
     list(
       fields = c("id","name","site"),
       id = project_id,
       show_searchers_and_regions = "1"
-    )   
+    )
   )
   add_head <- add_headers(.headers = c("Content-Type"="application/json","User-Id"=user_id,"Authorization"=token))
   answer <- POST("https://api.topvisor.com/v2/json/get/projects_2/projects",
@@ -40,31 +49,29 @@ TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 
   result <- data.frame(stringsAsFactors = F)
   dataRaw <- dataRaw$result
   # if (dataRaw[[1]]$searchers[[1]]$searcher==0) {
-  #   Ysearcher <- dataRaw[[1]]$searchers[[1]] 
-  #   Gsearcher <- dataRaw[[1]]$searchers[[2]] 
+  #   Ysearcher <- dataRaw[[1]]$searchers[[1]]
+  #   Gsearcher <- dataRaw[[1]]$searchers[[2]]
   # } else {
-  #   Ysearcher <- dataRaw[[1]]$searchers[[2]] 
-  #   Gsearcher <- dataRaw[[1]]$searchers[[1]] 
+  #   Ysearcher <- dataRaw[[1]]$searchers[[2]]
+  #   Gsearcher <- dataRaw[[1]]$searchers[[1]]
   # }
-  
+
   for (i in 1:length(dataRaw[[1]]$searchers))
-    for (ii in 1:length(dataRaw[[1]]$searchers[[i]]$regions))
-    {
+    for (ii in 1:length(dataRaw[[1]]$searchers[[i]]$regions)) {
       result <- rbind(result, c(dataRaw[[1]]$searchers[[i]]$searcher,dataRaw[[1]]$searchers[[i]]$name,dataRaw[[1]]$searchers[[i]]$regions[[ii]]$key,dataRaw[[1]]$searchers[[i]]$regions[[ii]]$name,dataRaw[[1]]$searchers[[i]]$regions[[ii]]$index,dataRaw[[1]]$searchers[[i]]$regions[[ii]]$device), stringsAsFactors = F)
     }
   colnames(result) <- c("searcher_key","searcher_name","region_key","region_name","region_index","device")
   regions <- result
   regions_keys <- subset(as.data.frame(unique(as.data.table(result), by = "region_key")), select = c("region_key","region_name","device"))
   searchers <- subset(as.data.frame(unique(as.data.table(result), by = "searcher_key")), select = c("searcher_key","searcher_name"))
-  
+
   datex = as.character(Sys.Date()-10000)
   datey = as.character(Sys.Date()-1)
-  
+
   #offset = 0
   #ldr = 3
   list_of_regions <- list()
-  for (i in 1:length(regions[[1]]))
-  {
+  for (i in 1:length(regions[[1]])) {
     list_of_regions[i] <- as.integer(regions$region_index[[i]])
   }
     body = toJSON(
@@ -75,39 +82,37 @@ TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 
         show_exists_dates = "1"
       #  limit = 10000,
       #  offset = offset
-      )   
+      )
     )
-    
+
     add_head <- add_headers(.headers = c("Content-Type"="application/json","User-Id"=user_id,"Authorization"=token))
     answer <- POST("https://api.topvisor.com/v2/json/get/positions_2/history",
                    body = body, add_head)
     dataRaw <- content(answer, "parsed", "application/json")
     existDates <- dataRaw$result$existsDates
-    
-   
-    
-    if (as.integer(difftime(as.Date(existDates[[1]]),as.Date(date1))) < 0) 
-    {
+
+    if (as.integer(difftime(as.Date(existDates[[1]]),as.Date(date1))) < 0) {
       exx <- as.data.frame(unlist(existDates))
       colnames(exx) <- c("name")
       existDates <- subset(existDates,as.integer(difftime(as.Date(exx[["name"]]),as.Date(date1))) >= 0)
+      exx <- as.data.frame(unlist(existDates))
+      colnames(exx) <- c("name")
+      existDates <- subset(existDates,as.integer(difftime(as.Date(exx[["name"]]),as.Date(date2))) <= 0)
     }
-    
-    
-    if (length(existDates) == 0)
-    {
+
+    if (length(existDates) == 0) {
       packageStartupMessage("DATES ARE NOT RIGHT OR NO MONEY FOR YESTERDAY", appendLF = T)
-      return(NULL)
-    } else {  
+      return (NULL)
+    } else {
   result <- data.frame(stringsAsFactors = F)
-  if (as.character(Sys.Date()) == existDates[[length(existDates)]]) existDates[[length(existDates)]] <- NULL
+ # if (as.character(Sys.Date()) == existDates[[length(existDates)]]) existDates[[length(existDates)]] <- NULL
   packageStartupMessage("Processing", appendLF = F)
   for (i in 1:length(existDates))
   {
     offset = 0
     ldr = 3
   while (ldr==3)
-  {    
+  {
     body = toJSON(
       list(
         fields = c("id","name","group_id","group_name"),
@@ -117,13 +122,13 @@ TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 
         type_range = 0,
         limit = 10000,
         offset = offset
-      )   
+      )
     )
     add_head <- add_headers(.headers = c("Content-Type"="application/json","User-Id"=user_id,"Authorization"=token))
     answer <- POST("https://api.topvisor.com/v2/json/get/positions_2/history",
                    body = body, add_head)
     dataRaw <- content(answer, "parsed", "application/json")
-    if (length(dataRaw)==3) offset <- dataRaw$nextOffset
+    if (length(dataRaw) == 3) offset <- dataRaw$nextOffset
     ldr <- length(dataRaw)
     dataRaw <- dataRaw$result$keywords
     for (ii in 1:length(dataRaw))
@@ -136,12 +141,12 @@ TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 
       }
     }
     packageStartupMessage(".", appendLF = F)
-    
+
    }
   }
   colnames(result) <- c("date","id","name","group_id","group_name","region_index","position")
-  
-  
+
+
   # result$region_id <- as.integer(result$region_id)
   #  regions$region_key <- as.integer(regions$region_key)
   result <-merge.data.frame(result, regions, by = c("region_index"))
@@ -150,6 +155,6 @@ TopVisorPos <- function (user_id = NULL, token = NULL, project_id = NULL, date1 
   packageStartupMessage("",appendLF = T)
   packageStartupMessage(" Processed ",length(result$position)," rows", appendLF = T)
   return(result)
-  
+
     }
 }
